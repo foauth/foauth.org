b74462808580857165df66fbee333596848570cb
Marty Alchin <marty@martyalchin.com>
2011-12-12 06:56:39 -0800

    A couple more providers

---
+++ services/evernote.py
@@ -0,0 +1,29 @@
+import foauth.providers
+import urlparse
+
+
+class Evernote(foauth.providers.OAuth1):
+    # General info about the provider
+    provider_url = 'https://www.evernote.com/'
+    favicon_url = 'http://www.evernote.com/about/media/img/favicon.ico'
+    docs_url = 'https://www.evernote.com/about/developer/api/'
+
+    # URLs to interact with the API
+    request_token_url = 'https://www.evernote.com/oauth'
+    authorize_url = 'https://www.evernote.com/OAuth.action'
+    access_token_url = 'https://www.evernote.com/oauth'
+    api_root = 'https://www.evernote.com/edam/note/'
+
+    signature_method = foauth.providers.SIGNATURE_HMAC
+    signature_location = foauth.prodivers.SIGN_HEADER
+
+    def get_access_token(self):
+        # Override standard token request to also get the data shard
+        data = super(Evernote, self).get_access_token()
+        self.shard = data['edam_shard']
+        return data
+
+    def get_api_root(self):
+        # Override standard API root to add the data shard
+        return urlparse.urljoin(self.api_root, self.shard + '/')
+
