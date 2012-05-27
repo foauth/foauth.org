bd235faab72032269218f4ae901cad05521c0fd1
Marty Alchin <marty@martyalchin.com>
2012-05-26 17:41:31 -0700

    The bulk of the app itself

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask
+
+from services import bitbucket
+from services import digg
+from services import dropbox
+from services import fitbit
+from services import flickr
+from services import twitter
+
+app = Flask(__name__)
+app.secret_key = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+def init_services(*services):
+    service_list = []
+
+    for service in services:
+        key = os.environ.get('%s_KEY' % service.alias, '').decode('utf8')
+        secret = os.environ.get('%s_SECRET' % service.alias, '').decode('utf8')
+
+        if key and secret: # Only initialize if all the pieces are in place
+            service_list.append(service(key, secret))
+
+    return service_list
+
+services = init_services(bitbucket.Bitbucket,
+                         digg.Digg,
+                         dropbox.Dropbox,
+                         fitbit.FitBit,
+                         flickr.Flickr,
+                         twitter.Twitter)
---
+++ forms.py
@@ -0,0 +1,33 @@
+from flaskext.wtf import Form, TextField, PasswordField, BooleanField, validators
+
+import models
+
+
+class Signup(Form):
+    email = TextField('Email', [
+        validators.Required('It&rsquo;s okay, we won&rsquo;t email you unless you want us to.'),
+        validators.Email('Um, that doesn&rsquo;t look like an email address.'),
+    ])
+    password = PasswordField('Password', [
+        validators.Required('How else will we know it&rsquo;s really you?'),
+        validators.EqualTo('retype', message='If you can&rsquo;t type it twice now, you&rsquo;ll never be able to login with it.')
+    ])
+    retype = PasswordField('(again)')
+    consent = BooleanField('Accept the Terms', [
+        validators.Required('Don&rsquo;t worry, they&rsquo;re really simple.')
+    ])
+
+    def validate_email(form, field):
+        if models.User.query.filter_by(email=field.data).count():
+            raise validators.ValidationError('Looks like you&rsquo;ve already registered. Try the login box at the top.')
+
+
+class Login(Form):
+    email = TextField('Email', validators=[
+        validators.Email('Please supply an email address.')
+    ])
+    password = PasswordField('Password', validators=[
+        validators.Required('Please supply a password.')
+    ])
+
+
