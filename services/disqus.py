c4fdd648c0d4c1ccddf5c0484861108a96ff0400
Marty Alchin <marty@martyalchin.com>
2012-05-31 22:09:29 -0700

    Adding Disqus

---
+++ services/disqus.py
@@ -0,0 +1,38 @@
+import json
+from oauthlib.oauth2.draft25 import utils
+import foauth.providers
+
+
+def token_uri(service, token, r):
+    params = [((u'access_token', token)), ((u'api_key', service.client_id))]
+    r.url =  utils.add_params_to_uri(r.url, params)
+    return r
+
+
+class Disqus(foauth.providers.OAuth2):
+    # General info about the provider
+    provider_url = 'http://disqus.com/'
+    favicon_url = 'http://mediacdn.disqus.com/1324593591/img/favicon.ico'
+    docs_url = 'http://disqus.com/api/docs/'
+
+    # URLs to interact with the API
+    authorize_url = 'https://disqus.com/api/oauth/2.0/authorize/'
+    access_token_url = 'https://disqus.com/api/oauth/2.0/access_token/'
+    api_domain = 'disqus.com'
+
+    available_permissions = [
+        (None, 'read data on your behalf'),
+        ('write', 'write data on your behalf'),
+        ('admin', 'moderate your forums'),
+    ]
+
+    bearer_type = token_uri
+
+    def get_scope_string(self, scopes):
+        # Disqus doesn't follow the spec on this point
+        return ','.join(scopes)
+
+    def parse_token(self, content):
+        data = json.loads(content)
+        return data['access_token']
+
