b12f8a8b153b94742879ebdb0eaae8d08cf7a169
Marty Alchin <marty@martyalchin.com>
2012-06-01 06:55:22 -0700

    Add Foursquare

--- services/foursquare.py
+++ services/foursquare.py
@@ -1,7 +1,9 @@
-import foauth
+import json
+from oauthlib.oauth2.draft25 import utils
+import foauth.providers
 
 
-class Foursquare(foauth.providers.Oauth2):
+class Foursquare(foauth.providers.OAuth2):
     # General info about the provider
     provider_url = 'http://www.foursquare.com/'
     favicon_url = 'https://static-s.foursquare.com/favicon-c62b82f6120e2c592a3e6f3476d66554.ico'
@@ -10,9 +12,16 @@ class Foursquare(foauth.providers.Oauth2):
     # URLs to interact with the API
     authorize_url = 'https://foursquare.com/oauth2/authorize'
     access_token_url = 'https://foursquare.com/oauth2/access_token'
-    api_root = 'https://api.foursquare.com/v2/'
+    api_domain = 'api.foursquare.com'
 
-    signature_method = foauth.providers.SIGNATURE_HMAC
-    signature_location = foauth.prodivers.SIGN_HEADER
+    available_permissions = [
+        (None, 'read and write to your check-ins'),
+    ]
 
+    def bearer_type(self, token, r):
+        r.url =  utils.add_params_to_uri(r.url, [((u'oauth_token', token))])
+        return r
 
+    def parse_token(self, content):
+        data = json.loads(content)
+        return data['access_token']
