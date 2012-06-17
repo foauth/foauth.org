b90958d64eb99281e176d65027fffe8ce230e354
Marty Alchin <marty@martyalchin.com>
2012-06-16 20:50:13 -0700

    Add MySpace
    In my tests, it's been timing out while trying to get the request token, but it might just be a temporary situation on their end.

---
+++ services/myspace.py
@@ -0,0 +1,19 @@
+import foauth.providers
+
+
+class MySpace(foauth.providers.OAuth1):
+    # General info about the provider
+    provider_url = 'http://www.myspace.com/'
+    favicon_url = 'http://www.myspace.com/favicon.ico'
+    docs_url = 'http://developerwiki.myspace.com/index.php?title=RESTful_API_Overview'
+
+    # URLs to interact with the API
+    request_token_url = 'http://api.myspace.com/request_token'
+    authorize_url = 'http://api.myspace.com/authorize'
+    access_token_url = 'http://api.myspace.com/access_token'
+    api_domain = 'api.myspace.com'
+
+    available_permissions = [
+        (None, 'read and write your social information'),
+    ]
+
