3cf623aa41f429b5e5e83829a7c74634908d97d1
Marty Alchin <marty@martyalchin.com>
2012-06-16 21:29:17 -0700

    Add UserVoice
    I don't know how this'll work, since it's not really a generic service,
    much like GetSatisfaction. But here it is, in case it works for someone else.

---
+++ services/uservoice.py
@@ -0,0 +1,16 @@
+import foauth.providers
+
+
+class UserVoice(foauth.providers.OAuth1):
+    # General info about the provider
+    name = 'UserVoice'
+    provider_url = 'http://uservoice.com/'
+    favicon_url = 'http://uservoice.com/favicon.ico'
+    docs_url = 'http://developer.uservoice.com/docs/api/reference/'
+
+    # URLs to interact with the API
+    request_token_url = 'http://uservoice.com/api/v1/oauth/access_token'
+    authorize_url = 'http://uservoice.com/api/v1/oauth/authorize'
+    access_token_url = 'http://uservoice.com/api/v1/oauth/request_token'
+    api_domain = 'uservoice.com'
+
