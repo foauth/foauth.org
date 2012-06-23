b0a376f8d725ba00a45be517bfb7e704cf9afa55
Marty Alchin <marty@martyalchin.com>
2012-06-23 14:37:40 -0700

    Fix Meetup

--- services/meetup.py
+++ services/meetup.py
@@ -10,7 +10,9 @@ class Meetup(foauth.providers.OAuth2):
     # URLs to interact with the API
     authorize_url = 'https://secure.meetup.com/oauth2/authorize'
     access_token_url = 'https://secure.meetup.com/oauth2/access'
-    api_domain = 'http://api.meetup.com/'
+    api_domain = 'api.meetup.com'
+
+    bearer_type = foauth.providers.BEARER_URI
 
     available_permissions = [
         (None, 'access your groups, create and edit events, and post photos'),
