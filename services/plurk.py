6942d4aeb1a04d6c1054c8f57af3213aef64b400
Marty Alchin <marty@martyalchin.com>
2012-07-27 22:47:04 -0700

    Plurk!
    Remember Plurk? Yeah, neither do I.

---
+++ services/plurk.py
@@ -0,0 +1,20 @@
+import foauth.providers
+
+
+class Plurk(foauth.providers.OAuth1):
+    # General info about the provider
+    provider_url = 'http://www.plurk.com/'
+    docs_url = 'http://www.plurk.com/API#toc'
+
+    # URLs to interact with the API
+    request_token_url = 'https://www.plurk.com/OAuth/request_token'
+    authorize_url = 'http://www.plurk.com/OAuth/authorize'
+    access_token_url = 'https://www.plurk.com/OAuth/access_token'
+    api_domain = 'www.plurk.com'
+
+    available_permissions = [
+        (None, 'access and update your profile and plurks'),
+    ]
+
+    https = False
+
