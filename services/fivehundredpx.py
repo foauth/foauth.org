3708109867ab0647f64ab9669fb1a4608aa72327
Marty Alchin <marty@martyalchin.com>
2012-08-02 06:53:47 -0700

    Add 500px

---
+++ services/fivehundredpx.py
@@ -0,0 +1,19 @@
+import foauth.providers
+
+
+class FiveHundredPX(foauth.providers.OAuth1):
+    # General info about the provider
+    alias = '500px'
+    name = '500px'
+    provider_url = 'http://500px.com/'
+    docs_url = 'https://github.com/500px/api-documentation'
+
+    # URLs to interact with the API
+    request_token_url = 'https://api.500px.com/v1/oauth/request_token'
+    authorize_url = 'https://api.500px.com/v1/oauth/authorize'
+    access_token_url = 'https://api.500px.com/v1/oauth/access_token'
+    api_domain = 'api.500px.com'
+
+    available_permissions = [
+        (None, 'access and manage your photos'),
+    ]
