0707b39fab0ee279ed81a94445b6ec1ebb5cbc55
Marty Alchin <marty@martyalchin.com>
2012-08-09 06:27:04 -0700

    Add imgur

---
+++ services/imgur.py
@@ -0,0 +1,18 @@
+import foauth.providers
+
+
+class Imgur(foauth.providers.OAuth1):
+    # General info about the provider
+    name = 'imgur'
+    provider_url = 'http://imgur.com/'
+    docs_url = 'http://api.imgur.com/'
+
+    # URLs to interact with the API
+    request_token_url = 'https://api.imgur.com/oauth/request_token'
+    authorize_url = 'https://api.imgur.com/oauth/authorize'
+    access_token_url = 'https://api.imgur.com/oauth/access_token'
+    api_domain = 'api.imgur.com'
+
+    available_permissions = [
+        (None, 'read and write to your images'),
+    ]
