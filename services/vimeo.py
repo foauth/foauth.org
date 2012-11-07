d26b9d22363f9763f959332d07445a2a4e7c221c
Marty Alchin <marty@martyalchin.com>
2012-11-06 22:09:17 -0800

    Rewrite Vimeo to use the new scope selection system

--- services/vimeo.py
+++ services/vimeo.py
@@ -9,15 +9,24 @@ class Vimeo(foauth.providers.OAuth1):
 
     # URLs to interact with the API
     request_token_url = 'https://vimeo.com/oauth/request_token'
-    authorize_url = 'https://vimeo.com/oauth/authorize?permission=delete'
+    authorize_url = 'https://vimeo.com/oauth/authorize'
     access_token_url = 'https://vimeo.com/oauth/access_token'
     api_domain = 'vimeo.com'
 
     available_permissions = [
-        ('read', 'access information about videos'),
-        ('write', 'update and like videos'),
-        ('delete', 'delete videos'),
+        (None, 'access your videos'),
+        ('write', 'access, update and like videos'),
+        ('delete', 'access, update, like and delete videos'),
     ]
+    permissions_widget = 'radio'
+
+    def get_authorize_params(self, redirect_uri, scopes):
+        params = super(Vimeo, self).get_authorize_params(redirect_uri, scopes)
+
+        if any(scopes):
+            params['permission'] = scopes[0]
+
+        return params
 
     def get_user_id(self, key):
         r = self.api(key, self.api_domain, u'/api/rest/v2?method=vimeo.people.getInfo&format=json')
