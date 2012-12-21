d18d832a109fe2415ecc638922479277e0df9389
Marty Alchin <marty@martyalchin.com>
2012-12-21 12:37:01 -0800

    Update imgur to version 3 of their API, including OAuth 2

--- services/imgur.py
+++ services/imgur.py
@@ -1,7 +1,7 @@
 import foauth.providers
 
 
-class Imgur(foauth.providers.OAuth1):
+class Imgur(foauth.providers.OAuth2):
     # General info about the provider
     name = 'imgur'
     provider_url = 'http://imgur.com/'
@@ -9,15 +9,17 @@ class Imgur(foauth.providers.OAuth1):
     category = 'Pictures'
 
     # URLs to interact with the API
-    request_token_url = 'https://api.imgur.com/oauth/request_token'
-    authorize_url = 'https://api.imgur.com/oauth/authorize'
-    access_token_url = 'https://api.imgur.com/oauth/access_token'
+    authorize_url = 'https://api.imgur.com/oauth2/authorize'
+    access_token_url = 'https://api.imgur.com/oauth2/token'
     api_domain = 'api.imgur.com'
 
     available_permissions = [
         (None, 'read and write to your images'),
     ]
 
+    # This is hopefully a short-term fix to a hostname mismatch on their end
+    verify = False
+
     def get_user_id(self, key):
-        r = self.api(key, self.api_domain, u'/2/account.json')
-        return unicode(r.json[u'account'][u'url'])
+        r = self.api(key, self.api_domain, u'/3/account/me.json')
+        return unicode(r.json[u'data'][u'id'])
