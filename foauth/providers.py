bdde1283ac372cac9ccb4b6878addfaa38e60a23
Marty Alchin <marty@martyalchin.com>
2013-01-09 21:08:29 -0800

    Only override auth when actually necessary

--- foauth/providers.py
+++ foauth/providers.py
@@ -181,6 +181,7 @@ class OAuth2(OAuth):
     token_type = BEARER
     bearer_type = BEARER_HEADER
     supports_state = True
+    auth = None
 
     def parse_token(self, content):
         return json.loads(content)
@@ -217,7 +218,7 @@ class OAuth2(OAuth):
             'grant_type': 'authorization_code',
             'code': data['code'],
             'redirect_uri': redirect_uri
-        }, verify=self.verify, auth=(self.client_id, self.client_secret))
+        }, verify=self.verify, auth=self.auth)
 
         return self.parse_token(resp.content)
 
@@ -227,7 +228,7 @@ class OAuth2(OAuth):
             'client_secret': self.client_secret,
             'grant_type': 'refresh_token',
             'refresh_token': token
-        }, verify=self.verify, auth=(self.client_id, self.client_secret))
+        }, verify=self.verify, auth=self.auth)
 
         return self.parse_token(resp.content)
 
