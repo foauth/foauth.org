a83bfead91bed6a4f1db74720f71efe78e57c82a
Marty Alchin <marty@martyalchin.com>
2013-03-02 23:43:42 -0800

    Update to v1.1 of Twitter's API

--- services/twitter.py
+++ services/twitter.py
@@ -48,5 +48,5 @@ class Twitter(foauth.providers.OAuth1):
         return super(Twitter, self).callback(data, *args, **kwargs)
 
     def get_user_id(self, key):
-        r = self.api(key, self.api_domain, u'/1/account/verify_credentials.json')
+        r = self.api(key, self.api_domain, u'/1.1/account/verify_credentials.json')
         return unicode(r.json()[u'id'])
