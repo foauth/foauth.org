5a4cf095a3eda5127ca54f8d293162740b836158
Marty Alchin <marty@martyalchin.com>
2013-12-21 12:39:10 -0800

    Rewrite Heroku's scope handling a bit to better match reality

--- services/heroku.py
+++ services/heroku.py
@@ -13,13 +13,19 @@ class Heroku(foauth.providers.OAuth2):
     api_domain = 'api.heroku.com'
 
     available_permissions = [
-        ('identity', 'read your account information'),
+        (None, 'read your account information'),
         ('read', 'read all of your apps and resources, excluding configuration values'),
         ('write', 'write to all of your apps and resources, excluding configuration values'),
         ('read-protected', 'read all of your apps and resources, including configuration values'),
         ('write-protected', 'write to all of your apps and resources, including configuration values'),
         ('global', 'read and write to all of your account, apps and resources'),
     ]
+    permissions_widget = 'radio'
+
+    def get_authorize_params(self, redirect_uri, scopes):
+        params = super(Heroku, self).get_authorize_params(redirect_uri, scopes)
+        params['scope'] = scopes[0] or 'identity'
+        return params
 
     def get_user_id(self, key):
         r = self.api(key, self.api_domain, u'/account')
