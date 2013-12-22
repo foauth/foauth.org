faaeb1ac11c20440f24925687f0841d1c0eab6ef
Marty Alchin <marty@martyalchin.com>
2013-12-22 13:04:49 -0800

    Supply rdio's authorization page directly

--- services/rdio.py
+++ services/rdio.py
@@ -6,12 +6,13 @@ import foauth.providers
 class Rdio(foauth.providers.OAuth1):
     # General info about the provider
     provider_url = 'http://www.rdio.com/'
+    favicon_url = 'http://www.rdio.com/media/favicon_rdio_2012_11_15.ico'
     docs_url = 'http://developer.rdio.com/docs/REST/'
     category = 'Music'
 
     # URLs to interact with the API
     request_token_url = 'http://api.rdio.com/oauth/request_token'
-    authorize_url = None  # Provided when the request token is granted
+    authorize_url = 'https://www.rdio.com/account/oauth1/authorize/'
     access_token_url = 'http://api.rdio.com/oauth/access_token'
     api_domain = 'api.rdio.com'
 
@@ -20,13 +21,6 @@ class Rdio(foauth.providers.OAuth1):
     ]
 
     https = False
-
-    def parse_token(self, content):
-        # Override standard token request to also get the authorization URL
-        data = url_decode(content)
-        if 'login_url' in data:
-            self.authorize_url = data['login_url']
-        return super(Rdio, self).parse_token(content)
 
     def get_user_id(self, key):
         r = self.api(key, self.api_domain, u'/1/', method='POST', data={
