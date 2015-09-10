ef661626b8d2bc17e31525e201c461e384521675
Cass Malloy <cass@runscope.com>
2015-09-09 17:17:56 -0700

    Only check both on services starting with digit

--- config.py
+++ config.py
@@ -35,8 +35,11 @@ for module_name in get_service_modules():
         alias = service.alias.upper()
         if alias[0].isdigit():
             underscore_alias = "_" + alias
-        key = os.environ.get('%s_KEY' % alias, '').decode or os.environ.get('%s_KEY' % underscore_alias, '').decode('utf8')
-        secret = os.environ.get('%s_SECRET' % alias, '').decode('utf8') or os.environ.get('%s_SECRET' % underscore_alias, '').decode('utf8')
+            key = os.environ.get('%s_KEY' % alias, '').decode('utf8') or os.environ.get('%s_KEY' % underscore_alias, '').decode('utf8')
+            secret = os.environ.get('%s_SECRET' % alias, '').decode('utf8') or os.environ.get('%s_SECRET' % underscore_alias, '').decode('utf8')
+        else:
+            key = os.environ.get('%s_KEY' % alias, '').decode('utf8')
+            secret = os.environ.get('%s_SECRET' % alias, '').decode('utf8')
         if key and secret:  # Only initialize if all the pieces are in place
             services.append(service(key, secret))
 
